b2ca8ffa3edc5799c91989adf892970b261c7d62
danielpollo <akilesdaniel_@hotmail.com>
2012-07-31 14:11:33 -0500

    sucursal

---
+++ AventuraSalvaje/src/com/aventurasalvaje/sucursal/SucursalCtrl.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.aventurasalvaje.sucursal;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Intbox;
+import org.zkoss.zul.Textbox;
+
+/**
+ * @author SDGA
+ *
+ */
+public class SucursalCtrl extends GenericForwardComposer {
+
+	private int  idUsuario=15;
+
+	private Button aceptar;
+	private Textbox textdireccion;
+	private Textbox textcorreo;
+	private Intbox texttelefono;
+	private Textbox textnombre;
+	private SucursalBo sucursalBo;
+	/**
+	 *
+	 *
+	 */
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		sucursalBo = new SucursalBo();
+
+	}
+
+	public void onClick$aceptar(){
+		
+		String direccion = textdireccion.getValue();
+		String correo=textcorreo.getValue();
+		int telefono=texttelefono.getValue();
+		String nombre=textnombre.getValue();
+
+		System.out.println(nombre);
+		if(correo=="" || telefono== 0 || nombre==""){
+			alert("Debes de colocar todos los requerimientos");
+		}
+		else{
+			long tele=telefono;
+
+			sucursalBo.save(nombre,correo,tele,direccion,idUsuario);
+		}
+
+
+	}
+
+
+
+}
